7304aff9bc75e5ffb6871a62fe3d6970bca71cbd
phlinhng <phlinhng@outlook.com>
2020-05-04 22:23:16 +0800

    return when not generating link

--- v2.sh
+++ v2.sh
@@ -207,7 +207,7 @@ install_v2ray() {
   read -p "生成订阅链接 (yes/no)? " linkConfirm
   case "${linkConfirm}" in
     y|Y|[yY][eE][sS] ) generate_link ;;
-    * ) break;;
+    * ) return 0;;
   esac
 }
 
