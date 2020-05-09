333ff43956a10db512ba0acbcc66cd10caa21b22
phlinhng <phlinhng@outlook.com>
2020-05-10 05:10:56 +0800

    fix unary operator

--- src/v2script.sh
+++ src/v2script.sh
@@ -357,9 +357,9 @@ check_status() {
   fi
 
   printf "电报代理:　 "
-  if [ -f "/usr/local/etc/v2script/config.json" ] &&  [ $(read_json /usr/local/etc/v2script/config.json '.v2ray.tlsHeader') == "" ] &&  [ $(read_json /usr/local/etc/v2script/config.json '.mtproto.secret') != "" ];then
+  if [ -f "/usr/local/etc/v2script/config.json" ] &&  [[ $(read_json /usr/local/etc/v2script/config.json '.v2ray.tlsHeader') == "" ]] && [[ $(read_json /usr/local/etc/v2script/config.json '.mtproto.secret') != "" ]];then
     colorEcho ${YELLOW} echo "tg://proxy?server=`curl -s https://api.ipify.org`&port=443&secret=$(read_json /usr/local/etc/v2script/config.json '.mtproto.secret')"
-  elif  [ -f "/usr/local/etc/v2script/config.json" ] &&  [ $(read_json /usr/local/etc/v2script/config.json '.mtproto.secret') != "" ];then
+  elif  [ -f "/usr/local/etc/v2script/config.json" ] &&  [[ $(read_json /usr/local/etc/v2script/config.json '.mtproto.secret') != "" ]];then
     colorEcho ${YELLOW} echo "tg://proxy?server=$(read_json /usr/local/etc/v2script/config.json '.v2ray.tlsHeader')&port=443&secret=$(read_json /usr/local/etc/v2script/config.json '.mtproto.secret')"
   else
     colorEcho ${YELLOW} "未设置"
