e3677a3b35a5b6513302aab1e25d94553f837895
phlinhng <phlinhng@outlook.com>
2020-05-10 04:40:46 +0800

    v1.0-alpha-ds

--- src/v2script.sh
+++ src/v2script.sh
@@ -2,17 +2,9 @@
 
 branch="beta"
 
-# /usr/local/etc/v2script ##config path
-# /usr/local/etc/v2script/tls-header ##domain for v2Ray
-# /usr/local/etc/v2script/subscription ##filename of main subscription
-# /usr/local/etc/v2script/Caddyfile ##config of caddy
-# /usr/local/etc/v2script/mtproto-header  ##domain for mtproto
-# /usr/local/etc/v2script/mtproto-secret  ##secret for mtproto
-
 # /usr/local/bin/v2script ##main
 # /usr/local/bin/v2sub ##subscription manager
-
-# todo: use /usr/local/etc/v2script/script.json as config
+# /usr/local/etc/v2script/config.json ##config
 
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
   sudoCmd="sudo"
@@ -86,6 +78,10 @@ read_json() {
   ${sudoCmd} jq --raw-output $1 $2 2>/dev/null | tr -d '\n'
 }
 
+write_json() {
+  jq -r "$2 = \"$3\"" $1 > tmp.$$.json && ${sudoCmd} mv tmp.$$.json $1
+}
+
 display_vmess() {
   if [ ! -d "/usr/bin/v2ray" ]; then
     colorEcho ${RED} "尚末安装v2Ray"
@@ -104,7 +100,8 @@ display_vmess() {
   echo ""
 
   json="{\"add\":\"${V2_DOMAIN}\",\"aid\":\"0\",\"host\":\"\",\"id\":\"${uuid}\",\"net\":\"\",\"path\":\"\",\"port\":\"443\",\"ps\":\"${V2_DOMAIN}:443\",\"tls\":\"tls\",\"type\":\"none\",\"v\":\"2\"}"
-  uri="$(printf "${json}" | base64)" # todo: add link to v2script config
+  uri="$(printf "${json}" | base64)"
+  jq -r ".sub.nodes[.sub.nodes| length] |= . \"vmess://${uri}\"" /usr/local/etc/v2script/config.json > tmp.$$.json && mv tmp.$$.json /usr/local/etc/v2script/config.json
 
   echo "vmess://${uri}" | tr -d '\n' && printf "\n"
 }
@@ -118,13 +115,13 @@ generate_link() {
     return 1
   fi
 
-  if [ -f "/usr/local/etc/v2script/subscription" ]; then
-    ${sudoCmd} rm -f /var/www/html/$(${sudoCmd} cat /usr/local/etc/v2script/subscription)
+  if [ "$(read_json /usr/local/etc/v2script/config.json '.sub.uri')" != "" ]; then
+    "$(write_json /usr/local/etc/v2script/config.json '.sub.uri' \"\")"
   fi
 
   #${sudoCmd} ${systemPackage} install uuid-runtime coreutils jq -y
   uuid="$(read_json /etc/v2ray/config.json '.inbounds[0].settings.clients[0].id')"
-  V2_DOMAIN="$(${sudoCmd} cat /usr/local/etc/v2script/tls-header | tr -d '\n')"
+  V2_DOMAIN="$(read_json /usr/local/etc/v2script/config.json '.v2ray.tlsHeader')"
 
   read -p "输入节点名称[留空则使用默认值]: " remark
 
@@ -138,7 +135,8 @@ generate_link() {
   sub="$(printf "vmess://${uri}" | tr -d '\n' | base64)"
 
   randomName="$(uuidgen | sed -e 's/-//g' | tr '[:upper:]' '[:lower:]' | head -c 16)" #random file name for subscription
-  printf "${randomName}" | ${sudoCmd} tee /usr/local/etc/v2script/subscription >/dev/null
+  write_json /usr/local/etc/v2script/config.json ".sub.uri" "\"${randomName}\""
+
   printf "${sub}" | tr -d '\n' | ${sudoCmd} tee -a /var/www/html/${randomName} >/dev/null
   echo "https://${V2_DOMAIN}/${randomName}" | tr -d '\n' && printf "\n"
 }
@@ -146,7 +144,7 @@ generate_link() {
 get_v2ray() {
   ${sudoCmd} ${systemPackage} install curl -y
   # install v2ray
-  curl -sSL https://install.direct/go.sh | ${sudoCmd} bash
+  curl -sL https://install.direct/go.sh | ${sudoCmd} bash
 }
 
 install_v2ray() {
@@ -161,59 +159,65 @@ install_v2ray() {
   ${sudoCmd} ${systemPackage} update
   ${sudoCmd} ${systemPackage} install curl git coreutils wget ntp jq uuid-runtime unzip -y
 
+  cd $(mktemp -d)
+  wget https://github.com/phlinhng/v2ray-tcp-tls-web/archive/${branch}.zip
+  unzip ${branch}.zip && rm -f ${branch}.zip ## will unzip the source to current path and remove the archive file
+
+  if [ ! -d "/usr/local/etc/v2script" ]; then
+    mkdir -p /usr/local/etc/v2script ## folder for scripts configuration
+  else if [ ! -f "/usr/local/etc/v2script/config.json" ]; then
+    wget -q https://raw.githubusercontent.com/phlinhng/v2ray-tcp-tls-web/${branch}/config/v2scirpt.json -O /usr/local/etc/v2script/config.json
+  fi
+
   # install v2ray-core
   if [ ! -d "/usr/bin/v2ray" ]; then
     get_v2ray
+    write_json  /usr/local/etc/v2script/config.json ".v2ray.installed" "true"
   fi
 
   # install tls-shunt-proxy
   if [ ! -f "/usr/local/bin/tls-shunt-proxy" ]; then
-    curl -sSL https://raw.githubusercontent.com/liberal-boy/tls-shunt-proxy/master/dist/install.sh | ${sudoCmd} bash
+    curl -sL https://raw.githubusercontent.com/liberal-boy/tls-shunt-proxy/master/dist/install.sh | ${sudoCmd} bash
     colorEcho ${GREEN} "tls-shunt-proxy is installed."
+  fi
+
   if [ -f "/etc/tls-shunt-proxy/config.yaml" ]; then # tls-shunt-proxy installed and config.yaml exists
-    ${sudoCmd} cat /etc/tls-shunt-proxy/config.yaml | ${sudoCmd} tee /etc/tls-shunt-proxy/config.yaml.bak
-    ${sudoCmd} cat /etc/tls-shunt-proxy/config.yaml | ${sudoCmd} tee config-new.yaml
-    cd $(mktemp -d)
-    sed -i "s/FAKEV2DOMAIN/${V2_DOMAIN}/g" config-new.yaml
-    sed -i "s/##V2RAY@//g" config-new.yaml
-    ${sudoCmd} /bin/cp -f config-new.yaml /etc/tls-shunt-proxy/config.yaml
+    ${sudoCmd} cat /etc/tls-shunt-proxy/config.yaml | ${sudoCmd} tee /etc/tls-shunt-proxy/config.yaml.bak >/dev/null
+    config_new="$(mktemp)"
+    ${sudoCmd} cat /etc/tls-shunt-proxy/config.yaml > ${config_new}
+    sed -i "s/FAKEV2DOMAIN/${V2_DOMAIN}/g" ${config_new}
+    sed -i "s/##V2RAY@//g" ${config_new}
+    ${sudoCmd} mv ${config_new} /etc/tls-shunt-proxy/config.yaml
+  else
+    ${sudoCmd} rm -rf /etc/tls-shunt-proxy && ${sudoCmd} mkdir -p /etc/tls-shunt-proxy
+    sed -i "s/##V2RAY@//g" ./config/config.yaml
+    sed -i "s/FAKEV2DOMAIN/${V2_DOMAIN}/g" ./config/config.yaml
+    ${sudoCmd} /bin/cp -f ./config/config.yaml /etc/tls-shunt-proxy/config.yaml
   fi
 
   # install docker
-  curl -sSL https://get.docker.com/ | ${sudoCmd} bash
+  curl -sL https://get.docker.com/ | ${sudoCmd} bash
   # install docker-compose
   #${sudoCmd} curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   #${sudoCmd}  chmod +x /usr/local/bin/docker-compose
 
-  cd $(mktemp -d)
-  git clone https://github.com/phlinhng/v2ray-tcp-tls-web.git
-  cd v2ray-tcp-tls-web
-  git checkout ${branch}
-  # wget https://github.com/phlinhng/v2ray-tcp-tls-web/archive/${branch}.zip
-  # unzip ${branch}.zip ## will unzip the source to current path
-
   # prevent some bug
   ${sudoCmd} rm -rf /etc/ssl/tls-shunt-proxy
-  ${sudoCmd} rm -rf /etc/tls-shunt-proxy
-  ${sudoCmd} mkdir -p /etc/tls-shunt-proxy
-  ${sudoCmd} rm -rf /usr/local/etc/v2script
   ${sudoCmd} rm -rf /var/www/html
 
-  # folder for scripts configuration
-  ${sudoCmd} mkdir -p /usr/local/etc/v2script
-
   # create config files
-  uuid=$(${sudoCmd} cat /etc/v2ray/config.json | jq --raw-output '.inbounds[0].settings.clients[0].id')
+  uuid="$(read_json /etc/v2ray/config.json '.inbounds[0].settings.clients[0].id')"
   sed -i "s/FAKEUUID/${uuid}/g" ./config/v2ray.json
-  sed -i "s/##V2RAY@//g" ./config/config.yaml
-  sed -i "s/FAKEV2DOMAIN/${V2_DOMAIN}/g" ./config/config.yaml
-  sed -i "s/FAKEV2DOMAIN/${V2_DOMAIN}/g" ./config/Caddyfile
-  printf "${V2_DOMAIN}" | tr -d '\n' | ${sudoCmd} tee /usr/local/etc/v2script/tls-header
-
-  # copy cofig files to respective path
   ${sudoCmd} /bin/cp -f ./config/v2ray.json /etc/v2ray/config.json
-  ${sudoCmd} /bin/cp -f ./config/config.yaml /etc/tls-shunt-proxy/config.yaml
-  ${sudoCmd} /bin/cp -f ./config/Caddyfile /usr/local/etc/Caddyfile
+  sed -i "s/FAKEV2DOMAIN/${V2_DOMAIN}/g" ./config/Caddyfile
+  /bin/cp -f ./config/Caddyfile /usr/local/etc/Caddyfile
+  write_json /usr/local/etc/v2script/config.json ".v2ray.tlsHeader" "\"${V2_DOMAIN}\""
+
+  # add new user and overwrite v2ray.service
+  # https://github.com/v2ray/v2ray-core/issues/1011
+  ${sudoCmd} useradd -d /etc/v2ray/ -M -s /sbin/nologin v2ray
+  ${sudoCmd} /bin/cp -f ./config/v2ray.service /etc/systemd/system/v2ray.service
+  ${sudoCmd} chown -R v2ray:v2ray /var/log/v2ray
 
   # choose and copy a random  template for dummy web pages
   template="$(curl -s https://raw.githubusercontent.com/phlinhng/web-templates/master/list.txt | shuf -n  1)"
@@ -225,6 +229,13 @@ install_v2ray() {
   # set crontab to auto update geoip.dat and geosite.dat
   (crontab -l 2>/dev/null; echo "0 7 * * * wget -q https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat -O /usr/bin/v2ray/geoip.dat >/dev/null >/dev/null") | ${sudoCmd} crontab -
   (crontab -l 2>/dev/null; echo "0 7 * * * wget -q https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat -O /usr/bin/v2ray/geosite.dat >/dev/null >/dev/null") | ${sudoCmd} crontab -
+
+  # stop nginx service for user who had used the old version of script
+  ${sudoCmd} systemctl disable nginx
+  ${sudoCmd} systemctl stop nginx
+
+  # kill process occupying port 80
+  ${sudoCmd} kill -9 $(lsof -t -i:80)
 
   # activate caddy
   ${sudoCmd} docker run -d --restart=always -v /usr/local/etc/Caddyfile:/etc/Caddyfile -v $HOME/.caddy:/root/.caddy -p 80:80 abiosoft/caddy
@@ -240,10 +251,6 @@ install_v2ray() {
   ${sudoCmd} systemctl restart tls-shunt-proxy
   ${sudoCmd} systemctl daemon-reload
 
-  # remove installation files
-  cd ..
-  rm -rf v2ray-tcp-tls-web
-
   colorEcho ${GREEN} "安装TCP+TLS+WEB成功!"
   display_vmess
 
@@ -256,7 +263,7 @@ install_v2ray() {
 
 rm_v2script() {
   ${sudoCmd} ${systemPackage} install curl -y
-  curl -sSL https://raw.githubusercontent.com/phlinhng/v2ray-tcp-tls-web/${branch}/tools/rm_v2script.sh | bash
+  curl -sL https://raw.githubusercontent.com/phlinhng/v2ray-tcp-tls-web/${branch}/tools/rm_v2script.sh | bash
   exit 0
 }
 
@@ -271,12 +278,26 @@ get_v2sub() {
   fi
 }
 
+display_mtproto() {
+  if [ -f "/usr/local/etc/v2script/config.json" ] &&  [ $(read_json /usr/local/etc/v2script/config.json '.v2ray.tlsHeader') == "" ] &&  [ $(read_json /usr/local/etc/v2script/config.json '.mtproto.secret') != "" ];then
+    echo "tg://proxy?server=`curl -s https://api.ipify.org`&port=443&secret=$(read_json /usr/local/etc/v2script/config.json '.mtproto.secret')"
+  else if  [ -f "/usr/local/etc/v2script/config.json" ] &&  [ $(read_json /usr/local/etc/v2script/config.json '.mtproto.secret') != "" ];then
+    echo "tg://proxy?server=$(read_json /usr/local/etc/v2script/config.json '.v2ray.tlsHeader')&port=443&secret=$(read_json /usr/local/etc/v2script/config.json '.mtproto.secret')"
+  fi
+}
+
 install_mtproto() {
-  if [ ! -f "/usr/local/etc/v2script/mtproto-header" ] && [ ! -f "/usr/local/etc/v2script/mtproto-secret" ]; then
+  if [ ! -f "/usr/local/etc/v2script/config.json" ] || [ -f "/usr/local/etc/v2script/config.json" &&  $(read_json /usr/local/etc/v2script/config.json '.mtproto.installed') == "false" ]; then
     ${sudoCmd} ${systemPackage} update
     ${sudoCmd} ${systemPackage} install curl -y
 
-    curl -sSL https://get.docker.com/ | ${sudoCmd} bash
+    if [ ! -d "/usr/local/etc/v2script" ]; then
+      ${sudoCmd} mkdir -p /usr/local/etc/v2script ## folder for scripts configuration
+    else if [ ! -f "/usr/local/etc/v2script/config.json" ]; then
+      wget -q https://raw.githubusercontent.com/phlinhng/v2ray-tcp-tls-web/${branch}/config/v2scirpt.json -O /usr/local/etc/v2script/config.json
+    fi
+
+    curl -sL https://get.docker.com/ | ${sudoCmd} bash
 
     # generate random header from txt files
     FAKE_TLS_HEADER="$(curl -s https://raw.githubusercontent.com/phlinhng/my-scripts/master/text/mainland_cdn.txt | shuf -n 1)"
@@ -285,26 +306,27 @@ install_mtproto() {
     # start mtproto ## reference https://raw.githubusercontent.com/9seconds/mtg/master/run.sh
     ${sudoCmd} docker run -d --restart=always --name mtg --ulimit nofile=51200:51200 -p 127.0.0.1:3128:3128 nineseconds/mtg:latest run "${secret}"
 
-    printf "${FAKE_TLS_HEADER}" | tr  -d '\n' | ${sudoCmd} tee /usr/local/etc/v2script/mtproto-header >/dev/null
-    printf "${secret}" | tr  -d '\n' | ${sudoCmd} tee /usr/local/etc/v2script/mtproto-secret >/dev/null
+    write_json  "/usr/local/etc/v2script/config.json" ".mtproto.installed" "true"
+    write_json "/usr/local/etc/v2script/config.json" ".mtproto.faketlsHeader" "\"${FAKE_TLS_HEADER}\""
+    write_json "/usr/local/etc/v2script/config.json" ".mtproto.secret" "\"${secret}\""
 
     if [ ! -f "/usr/local/bin/tls-shunt-proxy" ]; then # tls-shunt-proxy not installed
-      colorEcho ${BLUE} "tls-shunt-proxy not installed, start installation"
-      curl -sSL https://raw.githubusercontent.com/liberal-boy/tls-shunt-proxy/master/dist/install.sh | ${sudoCmd} bash
+      colorEcho ${BLUE} "tls-shunt-proxy is not installed. start installation"
+      curl -sL https://raw.githubusercontent.com/liberal-boy/tls-shunt-proxy/master/dist/install.sh | ${sudoCmd} bash
       colorEcho ${GREEN} "tls-shunt-proxy is installed."
-      cd $(mktemp -d)
+      config_new="$(mktemp)"
       # crate new config.yaml and overwrite whatever the current one exisits or not
-      wget -q https://raw.githubusercontent.com/phlinhng/v2ray-tcp-tls-web/${branch}/config/config.yaml
-      sed -i "s/##MTPROTO@//g" config.yaml
-      sed -i "s/FAKEMTDOMAIN/${FAKE_TLS_HEADER}/g" config.yaml
-      ${sudoCmd} /bin/cp -f config.yaml /etc/tls-shunt-proxy/config.yaml
+      wget -q https://raw.githubusercontent.com/phlinhng/v2ray-tcp-tls-web/${branch}/config/config.yaml -O "${config_new}"
+      sed -i "s/##MTPROTO@//g" ${config_new}
+      sed -i "s/FAKEMTDOMAIN/${FAKE_TLS_HEADER}/g" ${config_new}
+      ${sudoCmd} mv ${config_new} /etc/tls-shunt-proxy/config.yaml
     elif [ -f "/etc/tls-shunt-proxy/config.yaml" ]; then # tls-shunt-proxy installed and config.yaml exists
-      ${sudoCmd} cat /etc/tls-shunt-proxy/config.yaml | ${sudoCmd} tee /etc/tls-shunt-proxy/config.yaml.bak
-      cd $(mktemp -d)
-      ${sudoCmd} cat /etc/tls-shunt-proxy/config.yaml | ${sudoCmd} tee config-new.yaml
-      sed -i "s/##MTPROTO@//g" config-new.yaml
-      sed -i "s/FAKEMTDOMAIN/${FAKE_TLS_HEADER}/g" config-new.yaml
-      ${sudoCmd} /bin/cp -f config-new.yaml /etc/tls-shunt-proxy/config.yaml
+      ${sudoCmd} cat /etc/tls-shunt-proxy/config.yaml | ${sudoCmd} tee /etc/tls-shunt-proxy/config.yaml.bak >/dev/null
+      config_new="$(mktemp)"
+      ${sudoCmd} cat /etc/tls-shunt-proxy/config.yaml > ${config_new}
+      sed -i "s/##MTPROTO@//g" ${config_new}
+      sed -i "s/FAKEMTDOMAIN/${FAKE_TLS_HEADER}/g" ${config_new}
+      ${sudoCmd} mv ${config_new} /etc/tls-shunt-proxy/config.yaml
     fi
     
     # activate service
@@ -313,13 +335,10 @@ install_mtproto() {
     ${sudoCmd} systemctl enable tls-shunt-proxy
     ${sudoCmd} systemctl restart tls-shunt-proxy
     ${sudoCmd} systemctl daemon-reload
-    
     colorEcho ${GREEN} "电报代理设置成功!"
-    echo ""
-    echo "tg://proxy?server=$(${sudoCmd} cat /usr/local/etc/v2script/tls-header)&port=443&secret=$(${sudoCmd} cat /usr/local/etc/v2script/mtproto-secret)"
-  else
-    echo "tg://proxy?server=$(${sudoCmd} cat /usr/local/etc/v2script/tls-header)&port=443&secret=$(${sudoCmd} cat /usr/local/etc/v2script/mtproto-secret)"
   fi
+
+  display_mtproto
 }
 
 check_status() {
@@ -331,15 +350,17 @@ check_status() {
   fi
 
   printf "主订阅链接: "
-  if [ -f "/usr/local/etc/v2script/subscription" ] && [ -f "/usr/local/etc/v2script/tls-header" ]; then
-    colorEcho ${YELLO} "https://$(${sudoCmd} cat /usr/local/etc/v2script/tls-header)/$(${sudoCmd} cat /usr/local/etc/v2script/subscription)"
+  if [ -f "/usr/local/etc/v2script/config.json" ] && [ $(read_json /usr/local/etc/v2script/config.json '.v2ray.installed') == "true" ]; then
+    colorEcho ${YELLO} "https://$(read_json /usr/local/etc/v2script/config.json '.v2ray.tlsHeader')/$(read_json /usr/local/etc/v2script/config.json '.sub.uri')"
   else
     colorEcho ${YELLO} "尚未生成"
   fi
 
   printf "电报代理:　 "
-  if [ ! -f "/usr/local/etc/v2script/mtproto-header" ] && [ ! -f "/usr/local/etc/v2script/mtproto-secret" ]; then
-    colorEcho ${YELLOW} "tg://proxy?server=$(${sudoCmd} cat /usr/local/etc/v2script/tls-header)&port=443&secret=$(${sudoCmd} cat /usr/local/etc/v2script/mtproto-secret)"
+  if [ -f "/usr/local/etc/v2script/config.json" ] &&  [ $(read_json /usr/local/etc/v2script/config.json '.v2ray.tlsHeader') == "" ] &&  [ $(read_json /usr/local/etc/v2script/config.json '.mtproto.secret') != "" ];then
+    colorEcho ${YELLOW} echo "tg://proxy?server=`curl -s https://api.ipify.org`&port=443&secret=$(read_json /usr/local/etc/v2script/config.json '.mtproto.secret')"
+  else if  [ -f "/usr/local/etc/v2script/config.json" ] &&  [ $(read_json /usr/local/etc/v2script/config.json '.mtproto.secret') != "" ];then
+    colorEcho ${YELLOW} echo "tg://proxy?server=$(read_json /usr/local/etc/v2script/config.json '.v2ray.tlsHeader')&port=443&secret=$(read_json /usr/local/etc/v2script/config.json '.mtproto.secret')"
   else
     colorEcho ${YELLOW} "未设置"
   fi
@@ -362,7 +383,7 @@ check_status() {
 
 vps_tools() {
   ${sudoCmd} ${systemPackage} install curl -y
-  curl -sSL https://raw.githubusercontent.com/phlinhng/v2ray-tcp-tls-web/${branch}/tools/vps_tools.sh | bash
+  curl -sL https://raw.githubusercontent.com/phlinhng/v2ray-tcp-tls-web/${branch}/tools/vps_tools.sh | bash
   exit 0
 }
 
