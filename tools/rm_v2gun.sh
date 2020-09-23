7505e8de08fee0c7bfa05896e8734127acf9a7ee
phlinhng <phlinhng@outlook.com>
2020-09-24 01:13:18 +0800

    remove unused lines

--- tools/rm_v2gun.sh
+++ tools/rm_v2gun.sh
@@ -100,9 +100,6 @@ ${sudoCmd} systemctl reset-failed
 colorEcho ${BLUE} "Removing trojan-go files."
 ${sudoCmd} rm -rf /usr/bin/trojan-go
 ${sudoCmd} rm -rf /etc/trojan-go
-${sudoCmd} rm -rf /etc/ssl/trojan-go
-${sudoCmd} crontab -l | grep -v 'trojan-go/geoip.dat' | ${sudoCmd} crontab -
-${sudoCmd} crontab -l | grep -v 'trojan-go/geosite.dat' | ${sudoCmd} crontab -
 colorEcho ${GREEN} "Removed trojan-go successfully."
 
 ${sudoCmd} ${systemPackage} autoremove -y --purge
