fe3df2e6e8c8326e2476460b59e9ff7a2af8fe94
phlinhng <phlinhng@outlook.com>
2020-09-24 01:23:34 +0800

    fix syntax

--- src/v2gun.sh
+++ src/v2gun.sh
@@ -133,7 +133,7 @@ show_links() {
 }
 
 preinstall() {
-  if [[ release == "debian"]] || [ release == "ubuntu" ] || [[ systemPackage == "apt-get" ]]; then
+  if [[ release == "debian" ]] || [[ release == "ubuntu" ]] || [[ systemPackage == "apt-get" ]]; then
     ${sudoCmd} ${systemPackage} update
     ${sudoCmd} ${systemPackage} install software-properties-common -y -q
     ${sudoCmd} add-apt-repository ppa:ondrej/nginx-mainline -y
