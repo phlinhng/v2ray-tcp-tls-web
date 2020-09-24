4c32430b8cb9b0e9d081f44e18be736ad4861610
phlinhng <phlinhng@outlook.com>
2020-09-24 17:15:53 +0800

    amd64 -> $MACHINE

--- src/v2gun.sh
+++ src/v2gun.sh
@@ -241,7 +241,7 @@ get_trojan() {
     colorEcho ${BLUE} "Getting the latest version of trojan-go"
     local latest_version="$(curl -s "https://api.github.com/repos/p4gefau1t/trojan-go/releases" | jq '.[0].tag_name' --raw-output)"
     echo "${latest_version}"
-    local trojango_link="https://github.com/p4gefau1t/trojan-go/releases/download/${latest_version}/trojan-go-linux-amd64.zip"
+    local trojango_link="https://github.com/p4gefau1t/trojan-go/releases/download/${latest_version}/trojan-go-linux-${MACHINE}.zip"
 
     cd $(mktemp -d)
     wget -nv "${trojango_link}" -O trojan-go.zip
