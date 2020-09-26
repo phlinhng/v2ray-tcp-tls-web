0ec2815feca92054b21fda7b2f2f202e75ea8528
phlinhng <phlinhng@outlook.com>
2020-09-26 16:58:37 +0800

    fix centos redirect

--- src/v2gun.sh
+++ src/v2gun.sh
@@ -367,13 +367,42 @@ server {
 }
 EOF
   elif [ -d "/etc/nginx/conf.d" ];then # centos
-    ${sudoCmd} cat > /etc/nginx/conf.d/default <<-EOF
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    server_name _;
-    return 301 https://\$host\$request_uri;
+    ${sudoCmd} cat > /etc/nginx/nginx.conf <<-EOF
+user nginx;
+worker_processes auto;
+error_log /var/log/nginx/error.log;
+pid /run/nginx.pid;
+include /usr/share/nginx/modules/*.conf;
+
+events {
+    worker_connections 1024;
 }
+
+http {
+    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile            on;
+    tcp_nopush          on;
+    tcp_nodelay         on;
+    keepalive_timeout   65;
+    types_hash_max_size 2048;
+
+    include             /etc/nginx/mime.types;
+    default_type        application/octet-stream;
+
+    include /etc/nginx/conf.d/*.conf;
+
+    server {
+        listen       80 default_server;
+        listen       [::]:80 default_server;
+        server_name  _;
+        return 301 https://\$host\$request_uri;
+    }
+  }
 EOF
   fi
 }
