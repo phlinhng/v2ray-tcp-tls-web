401363e37ad3014adb667572e0d0f9b42c6d7632
phlinhng <phlinhng@outlook.com>
2021-03-08 19:57:15 +0800

    fix layout

--- src/xwall.sh
+++ src/xwall.sh
@@ -137,7 +137,7 @@ build_web() {
 
 checkIP() {
   local realIP4="$(curl -sL ${ip4_api} -m 5)"
-  local resolvedIP4="$(curl https://cloudflare-dns.com/dns-query\?name\=$1\&type\=A -sSL -H 'accept: application/dns-json' | jq ".Answer[0].data" --raw-output)"
+  local resolvedIP4="$(curl -sSL https://cloudflare-dns.com/dns-query\?name\=$1\&type\=A -H 'accept: application/dns-json' | jq ".Answer[0].data" --raw-output)"
 
   [ ! -z "${realIP4}" ] && printf "%s %s\n" "detected IPv4 address:" "${realIP4}" | writeLog >> $log_path
   [ ! -z "${resolvedIP4}" ] && printf "%s %s\n" "found A record:" "${resolvedIP4}" | writeLog >> $log_path
@@ -147,7 +147,7 @@ checkIP() {
     return 0
   else
     local realIP6="$(curl -sL ${ip6_api} -m 5)"
-    local resolvedIP6="$(curl https://cloudflare-dns.com/dns-query\?name\=$1\&type\=AAAA -sSL -H 'accept: application/dns-json' | jq ".Answer[0].data" --raw-output)"
+    local resolvedIP6="$(curl -sSL https://cloudflare-dns.com/dns-query\?name\=$1\&type\=AAAA -H 'accept: application/dns-json' | jq ".Answer[0].data" --raw-output)"
     [ ! -z "${realIP6}" ] && printf "%s %s\n" "detected IPv6 address:" "${realIP6}" | writeLog >> $log_path
     [ ! -z "${resolvedIP6}" ] && printf "%s %s\n" "found AAAA record:" "${resolvedIP6}" | writeLog >> $log_path
     if [[ "${realIP6}" == "${resolvedIP6}" ]]; then
